8b0fca9b591fa84a82c486b7e67b2e1bf2827fe8
Karan Singhal <karan1149@gmail.com>
2019-05-30 14:36:44 -0700

    Code to encode words using saved pretrained word encoder

--- encode_words.py
+++ encode_words.py
@@ -2,6 +2,11 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from tqdm import tqdm
+import karantools as kt
+import encoders
+import utils
+import pickle
 
 from torchtext.vocab import GloVe
 
@@ -17,10 +22,36 @@ def get_word_vector(word):
 
 def main():
 	words = sorted(glove.stoi.keys())
-	print(words[:10])
-	print(len(words))
-	
 
+	left_encoder = encoders.FCEncoder((300, 300))
+	right_encoder = encoders.FCEncoder((2048, 300))
+
+	pair_encoder = encoders.PairEncoder(left_encoder, right_encoder)
+
+	pair_encoder.load_state_dict(torch.load('outputs/pair_encoder.pkl'))
+	word_encoder = pair_encoder.left_encoder
+	word_encoder.eval()
+
+	aligned_stoi = {word:i for i, word in enumerate(words)}
+	aligned_vectors = torch.zeros(len(words), VEC_SIZE)
+
+	with torch.no_grad():
+	
+		for i, word in tqdm(enumerate(words), total=len(words)):
+			word_vec = get_word_vector(word)
+
+			word_tensor = torch.unsqueeze(torch.as_tensor(word_vec), 0)
+
+			kt.assert_eq(word_tensor.shape, (1, VEC_SIZE))
+
+			aligned_vectors[i] = word_encoder(word_tensor)
+
+	aligned_vectors = aligned_vectors.numpy()
+
+	with open('outputs/encoded_words.pkl', 'wb') as f:
+		pickle.dump([aligned_stoi, aligned_vectors], f)
+
+	utils.stoi_vectors_to_txt(aligned_stoi, aligned_vectors, 'outputs/encoded_words.txt')
 
 if __name__=='__main__':
 	main()

da59ffa56512c045dc31f9a08ff68adf5fc496ec
Karan Singhal <karan1149@gmail.com>
2019-06-08 18:35:04 -0700

    5-way Concreteness prediction using MLP

--- run_concreteness_prediction.py
+++ run_concreteness_prediction.py
@@ -9,6 +9,7 @@ random.seed(42)
 import functools
 from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.linear_model import LogisticRegression
+from sklearn.neural_network import MLPClassifier
 
 def main(args):
 	with open('evaluation/concreteness/concreteness_data.pkl', 'rb') as f:
@@ -17,23 +18,23 @@ def main(args):
 	word_stoi, word_vectors = kt.lazy_load(functools.partial(utils.txt_to_stoi_vectors, args.filename), args.pkl_path)
 
 	word_vector_words = set(word_stoi.keys())
+	concreteness_words = set(concreteness_stoi.keys())
 
-	abstract_word_intersection = word_vector_words.intersection(abstract_words)
-	concrete_word_intersection = word_vector_words.intersection(concrete_words)
+	# abstract_word_intersection = word_vector_words.intersection(abstract_words)
+	# concrete_word_intersection = word_vector_words.intersection(concrete_words)
 
-	print('Abstract words', len(abstract_words))
-	print('Concrete words', len(concrete_words))
+	# print('Abstract words', len(abstract_words))
+	# print('Concrete words', len(concrete_words))
 
-	print('Abstract word intersection with word vectors', len(abstract_word_intersection))
-	print('Concrete word intersection with word vectors', len(concrete_word_intersection))
+	# print('Abstract word intersection with word vectors', len(abstract_word_intersection))
+	# print('Concrete word intersection with word vectors', len(concrete_word_intersection))
+
+	intersection_words = word_vector_words.intersection(concreteness_words)
 
 	X_y_pairs = []
 
-	for word in abstract_word_intersection:
-		X_y_pairs.append((word_vectors[word_stoi[word]], 0))
-
-	for word in concrete_word_intersection:
-		X_y_pairs.append((word_vectors[word_stoi[word]], 1))
+	for word in intersection_words:
+		X_y_pairs.append((word_vectors[word_stoi[word]], round(ratings[concreteness_stoi[word]])))
 
 	random.shuffle(X_y_pairs)
 	split_idx = int(len(X_y_pairs) * .9)
@@ -43,7 +44,7 @@ def main(args):
 	train_X, train_y = zip(*train_X_y_pairs)
 	test_X, test_y = zip(*test_X_y_pairs)
 
-	model = LogisticRegression()
+	model = MLPClassifier(early_stopping=True)
 	model.fit(train_X, train_y)
 
 	train_y_pred = model.predict(train_X)
